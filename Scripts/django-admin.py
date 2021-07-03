80bd16bf7270a55021e9ed07dee311b954f82a3d
Bartosz Kowalski <bartosz.t.kowalski@student.put.poznan.pl>
2021-07-03 11:36:38 +0200

    fix: delete unnecessary files

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!E:\Users\bartk\OneDrive\Semestr 6\Podstawy aplikacji internetowych\Projekt\ChessTournamentWebsite\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
